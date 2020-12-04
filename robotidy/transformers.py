6c52a6dbfc46d61d63be2463de4fddba32f9d6a7
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-12-04 14:39:49 +0100

    use __name__ instead of name class attr

--- robotidy/transformers.py
+++ robotidy/transformers.py
@@ -9,9 +9,8 @@ def load_transformers(modes):
     transfomers = []
     classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
     for transfomer_class in classes:
-        transfomer = transfomer_class[1]()
-        if getattr(transfomer, 'name', None) in modes:
-            transfomers.append(transfomer)
+        if transfomer_class[1].__name__ in modes:
+            transfomers.append(transfomer_class[1]())
     return transfomers
 
 
@@ -19,20 +18,14 @@ def load_transfomers_names():
     transfomers = []
     classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
     for transfomer_class in classes:
-        if hasattr(transfomer_class[1], 'name'):
-            transfomers.append(transfomer_class[1].name)
+        if issubclass(transfomer_class[1], ModelTransformer) and transfomer_class[1].__name__ != 'ModelTransformer':
+            transfomers.append(transfomer_class[1].__name__)
     return transfomers
 
 
 class DummyTransformer(ModelTransformer):
-    name = 'dummy_transformer'
-
-    def __str__(self):
-        return self.name
+    pass
 
 
-class AnotherTransfomer(ModelTransformer):
-    name = 'another_transformer'
-
-    def __str__(self):
-        return self.name
+class AnotherTransformer(ModelTransformer):
+    pass
