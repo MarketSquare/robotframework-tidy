08b85563cbc0365c9c02fee2521535c370837535
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-12-08 16:35:30 +0100

    add example tests

---
+++ tests/utest/test_decorators.py
@@ -0,0 +1,36 @@
+import pytest
+
+from robotidy.decorators import transformer, configurable
+
+
+class TestDecorators:
+    def test_transform_decorator(self):
+        @transformer
+        class TestingTransformer:
+            pass
+        assert getattr(TestingTransformer, 'is_transformer', False)
+
+    def test_configurable_init(self):
+        @transformer
+        class TestingTransformer:
+            def __init__(self):
+                self.some_value = 10
+
+            @configurable
+            def some_value(self, value):
+                return value + 2
+        transform = TestingTransformer()
+        assert getattr(transform, 'some_value', 0) == 12
+
+    def test_configurable_override(self):
+        @transformer
+        class TestingTransformer:
+            def __init__(self):
+                self.some_value = 10
+
+            @configurable
+            def some_value(self, value):
+                return value + 2
+        transform = TestingTransformer()
+        transform.some_value = 5
+        assert getattr(transform, 'some_value', 0) == 7
