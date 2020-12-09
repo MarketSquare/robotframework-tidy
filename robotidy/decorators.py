136f57cf345ac63f6df3a0ce3e3bf27203144c06
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-12-09 13:37:48 +0100

    extended configurable to accept default argument

--- robotidy/decorators.py
+++ robotidy/decorators.py
@@ -1,23 +1,11 @@
 import inspect
 
 
-class configurable:  # noqa
-    """
-    Decorator to expose method as configurable parameter.
-    Decorated method should return value.
-
-    Example::
-
-        @configurable
-        def some_value(self, value):
-            # parsing for value
-            return value
-
-        def other_method(self, arg):
-           print(self.some_value)
-    """
-    def __init__(self, fun):
+class ConfigurableDecorator:
+    def __init__(self, fun, **kwargs):
         self.fun = fun
+        if 'default' in kwargs:
+            self._value = kwargs['default']
 
     def __set_name__(self, owner, name):
         if not hasattr(owner, 'configurables'):
@@ -35,6 +23,41 @@ class configurable:  # noqa
             return self._value
         except AttributeError:
             raise AttributeError(f'{owner.__name__}.{self.fun.__name__} attribute was not initialized before use')
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+def configurable(function=None, **kwargs):
+    """
+    Decorator to expose method as configurable parameter.
+    Decorated method should return value.
+
+    Example::
+
+        @configurable
+        def some_value(self, value):
+            # parsing for value
+            return value
+
+        def other_method(self, arg):
+           print(self.some_value)
+
+    It is possible to pass default value with `default=` argument::
+
+        @configurable(default=10)
+
+    If the same value is initialized also in `__init__` it will be overridden in `__init__`.
+    The main difference is that if you set it in `__init__` it will go through parsing method (`some_value`).
+    On the other hand `default=` saves value directly.
+    """
+    if function:
+        return ConfigurableDecorator(function)
+    else:
+        def wrapper(function):
+            return ConfigurableDecorator(function, **kwargs)
+
+        return wrapper
 
 
 def transformer(arg=None):
--- tests/utest/test_decorators.py
+++ tests/utest/test_decorators.py
@@ -27,13 +27,37 @@ class TestDecorators:
         transform = TestingTransformer()
         assert getattr(transform, 'some_value', 0) == 12
 
-    def test_configurable_override(self):
+    def test_configurable_with_default(self):
+        @transformer
+        class TestingTransformer:
+            @configurable(default=15)
+            def some_value(self, value):
+                return value + 2
+        transform = TestingTransformer()
+        assert getattr(transform, 'some_value', 0) == 15
+
+    def test_configurable_with_default_and_init(self):
+        """First the value is set to 15 directly. Then in `__init__` some_value(10) is called which returns 10 + 2. """
         @transformer
         class TestingTransformer:
             def __init__(self):
                 self.some_value = 10
 
-            @configurable
+            @configurable(default=15)
+            def some_value(self, value):
+                return value + 2
+        transform = TestingTransformer()
+        assert getattr(transform, 'some_value', 0) == 12
+
+    def test_configurable_override(self):
+        """ Any default value should be overriden by direct assigment
+        (it should call some_value(5) which returns 5+2) """
+        @transformer
+        class TestingTransformer:
+            def __init__(self):
+                self.some_value = 10
+
+            @configurable(default=15)
             def some_value(self, value):
                 return value + 2
         transform = TestingTransformer()
