81caff159ee0e06d51d58ebb61628146ecd65936
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-12-13 18:22:38 +0100

    remove pytest-mock dependency

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'Click>=7.0'
     ],
     extras_requires={
-        'dev': ['pytest', 'pytest-mock', 'pylama', 'pylama_pylint', 'coverage'],
+        'dev': ['pytest', 'pylama', 'pylama_pylint', 'coverage'],
         'doc': ['sphinx', 'sphinx_rtd_theme']
     },
     entry_points={'console_scripts': ['robotidy=robotidy.cli:cli']},
