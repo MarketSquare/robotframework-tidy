47fb25623b28c9c0f139d647fb1c8d1098779701
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-12-13 19:21:38 +0100

    modify test data paths to allow running independently

--- tests/atest/transformers/test_transformers.py
+++ tests/atest/transformers/test_transformers.py
@@ -13,7 +13,8 @@ from robotidy.utils import decorate_diff_with_color
 def save_tmp_model(self, model):
     """ Decorator that disables default robotidy save to file mechanism and replace with mocked one.
     That way we can save output to 'actual' directory for easy comparison with expected files.  """
-    path = Path('actual', Path(model.source).name)
+    path = Path(Path(__file__).parent, 'actual', Path(model.source).name)
+    print(path)
     model.save(output=path)
 
 
@@ -21,12 +22,12 @@ def run_tidy(transformer_name: str, args: List[str] = None, sources: List[str] =
     runner = CliRunner()
     arguments = args if args is not None else []
     if sources is None:
-        paths = [str(Path(transformer_name, 'source'))]
+        paths = [str(Path(Path(__file__).parent, transformer_name, 'source'))]
     else:
-        paths = [str(Path(transformer_name, 'source', source)) for source in sources]
+        paths = [str(Path(Path(__file__).parent, transformer_name, 'source', source)) for source in sources]
     cmd = arguments + paths
     result = runner.invoke(cli, cmd)
-    if result.exit_code == exit_code:
+    if result.exit_code != exit_code:
         print(result.output)
         raise AssertionError(f'robotidy exit code: {result.exit_code} does not match expected: {exit_code}')
     return result
@@ -35,8 +36,8 @@ def run_tidy(transformer_name: str, args: List[str] = None, sources: List[str] =
 def compare_file(transformer_name: str, actual_name: str, expected_name: str = None):
     if expected_name is None:
         expected_name = actual_name
-    expected = Path(transformer_name, 'expected', expected_name)
-    actual = Path('actual', actual_name)
+    expected = Path(Path(__file__).parent, transformer_name, 'expected', expected_name)
+    actual = Path(Path(__file__).parent, 'actual', actual_name)
     if not filecmp.cmp(expected, actual):
         display_file_diff(expected, actual)
         raise AssertionError(f'File {actual_name} is not same as expected')
