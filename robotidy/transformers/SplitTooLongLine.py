96f67972763651e54b3b0ea935a96313071d6045
Blazej Michalik <blazej.michalik@nokia.com>
2021-02-05 01:51:33 +0100

    SplitTooLongLine v2: simpler implementation

--- robotidy/transformers/SplitTooLongLine.py
+++ robotidy/transformers/SplitTooLongLine.py
@@ -1,5 +1,3 @@
-from itertools import tee
-
 from robot.api.parsing import (
     ModelTransformer,
     Token
@@ -7,11 +5,14 @@ from robot.api.parsing import (
 
 
 EOL = Token(Token.EOL)
+CONTINUATION = Token(Token.CONTINUATION)
 
 
 class SplitTooLongLine(ModelTransformer):
-    def __init__(self, line_length: int = 120):
+    def __init__(self, line_length: int = 120, split_on_every_arg: bool = True):
+        super().__init__()
         self.line_length = line_length
+        self.split_on_every_arg = split_on_every_arg
 
     def visit_KeywordCall(self, node):  # noqa
         if all(line[-1].end_col_offset < self.line_length for line in node.lines):
@@ -20,78 +21,46 @@ class SplitTooLongLine(ModelTransformer):
 
     def split_keyword_call(self, node):
         separator = Token(Token.SEPARATOR, self.formatting_config.space_count * ' ')
-
-        is_single_line = node.lineno == node.end_lineno
-
         indent = node.tokens[0]
-        assignment = list(self.insert_seperator(node.get_tokens(Token.ASSIGN), separator))
+
+        assignment = node.get_token(Token.ASSIGN)
         keyword = node.get_token(Token.KEYWORD)
 
-        if is_single_line:
-            comment = node.get_tokens(Token.COMMENT)
-            comment_line = [indent] + comment + [EOL] if comment else []
-
-            head = comment_line + [indent] + assignment + [keyword]
-
-            tail = [token for token in node.tokens[node.tokens.index(keyword) + 1:]
-                    if token.type != Token.COMMENT]
-
-            lines = []
-            for tokenized_line in self.pack_arguments(head, tail, indent, separator):
-                lines.extend(tokenized_line)
-
+        if assignment:
+            head = [indent, assignment, separator, keyword]
         else:
-            return node  # Not implemented yet
+            head = [indent, keyword]
 
-        node.tokens = lines
+        comments, tail, line = [], [], []
+
+        rest = node.tokens[node.tokens.index(keyword) + 1:]
+        breakpoint()
+        for token in rest:
+            if token.type in {Token.EOL, Token.SEPARATOR, Token.CONTINUATION}:
+                continue
+            elif token.type == Token.COMMENT:
+                comments += [indent, token, EOL]
+            elif token.type == Token.ARGUMENT:
+                if self.cols_remaining(line + [separator, token]) == 0:
+                    line.append(EOL)
+                    tail += line
+                    line = [indent, CONTINUATION, separator, token]
+                else:
+                    line += [separator, token]
+            else:
+                raise RuntimeError(f"Token with an unrecognized type: {repr(token)}")
+
+        # last line
+        line.append(EOL)
+        tail += line
+
+        node.tokens = comments + head + tail
         return node
 
-    def pack_arguments(self, tokens_so_far, remaining_tokens, indent, separator):
-        if not remaining_tokens:
-            return []
-
-        # Ensure we are in the correct place to pack
-        assert (not remaining_tokens or (
-            remaining_tokens[0].type == Token.SEPARATOR
-            and remaining_tokens[1].type == Token.ARGUMENT
-        ))
-        assert tokens_so_far[-1].type != Token.EOL
-
-        line = tokens_so_far[:]
-
-        for token, next_token in self.lookahead(remaining_tokens):
-            if token.type in (Token.SEPARATOR, Token.CONTINUATION, Token.EOL):
-                continue
-
-            next_tokens = (
-                [separator, token, separator, next_token]
-                if next_token.type == Token.COMMENT else
-                [separator, token]
-            )
-
-            if self.cols_remaining(line + next_tokens) == 0:
-                line.append(EOL)
-                yield line
-                line = [indent, Token(Token.CONTINUATION)]
-
-            line.extend(next_tokens)
-
-        line.append(EOL)
-        yield line  # last line
-
     def cols_remaining(self, tokens):
+        if self.split_on_every_arg:
+            return 0
         return max(self.line_length - self.len_token_text(self.last_line_of(tokens)), 0)
-
-    @staticmethod
-    def lookahead(iterable):
-        iterable, shifted_by_one = tee(iterable)
-        next(shifted_by_one, None)
-        return zip(iterable, shifted_by_one)
-
-    def insert_seperator(self, iterator, separator):
-        for elem in iterator:
-            yield elem
-            yield separator
 
     @staticmethod
     def len_token_text(tokens):
