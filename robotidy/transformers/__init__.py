747e2faf1e5c8518a93c7a931def3a65a81c4c43
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2021-05-31 18:36:56 +0200

    add non default transformer (#71)

--- robotidy/transformers/__init__.py
+++ robotidy/transformers/__init__.py
@@ -4,6 +4,9 @@ Transformers are classes used to transform passed Robot Framework code model.
 To create your own transformer you need to create file with the same name as your transformer class. Your class
 need to inherit from ``ModelTransformer`` or ``ast.NodeTransformer`` class. Finally put name of your transformer in
 ``TRANSFORMERS`` variable in this file.
+
+If you don't want to run your transformer by default and only when calling robotidy with --transform YourTransformer
+then add ``ENABLED = False`` class attribute inside.
 """
 from robot.utils.importer import Importer
 
@@ -23,15 +26,18 @@ TRANSFORMERS = frozenset((
 
 def load_transformers(allowed_transformers):
     """ Dynamically load all classes from this file with attribute `name` defined in allowed_transformers """
+    loaded_transformers = dict()
     if allowed_transformers:
-        loaded_transformers = dict()
+
         for name, args in allowed_transformers:
-            name = f'robotidy.transformers.{name}' if name in TRANSFORMERS else name
+            import_name = f'robotidy.transformers.{name}' if name in TRANSFORMERS else name
             loaded_transformers[name] = Importer().import_class_or_module(
-                name,
+                import_name,
                 instantiate_with_args=args
             )
-        return loaded_transformers
     else:
-        return {name: Importer().import_class_or_module(f'robotidy.transformers.{name}', instantiate_with_args=())
-                for name in TRANSFORMERS}
+        for name in TRANSFORMERS:
+            imported_class = Importer().import_class_or_module(f'robotidy.transformers.{name}', instantiate_with_args=())
+            if getattr(imported_class, 'ENABLED', True):
+                loaded_transformers[name] = imported_class
+    return loaded_transformers
