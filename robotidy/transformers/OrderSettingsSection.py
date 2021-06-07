4507fa2c9185da21dff3f5d8d9d0102c44835d3e
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-06-07 16:06:09 +0200

    add docs

--- robotidy/transformers/OrderSettingsSection.py
+++ robotidy/transformers/OrderSettingsSection.py
@@ -15,6 +15,25 @@ class OrderSettingsSection(ModelTransformer):
     """
     Order settings inside *** Settings *** section.
 
+    Settings are grouped in following groups:
+      - documentation (Documentation, Metadata),
+      - imports (Library, Resource, Variables),
+      - settings (Suite Setup and Teardown, Test Setup and Teardown, Test Timeout, Test Template),
+      - tags (Force Tags, Default Tags)
+
+    Then ordered by groups (according to ``group_order = documentation,imports,settings,tags`` order). Every
+    group is separated by ``new_lines_between_groups = 1`` new lines.
+    Settings are grouped inside group. Default order can be modified through following parameters:
+      - ``documentation_order = documentation,metadata``
+      - ``imports_order = library,resource,variables``
+      - ``settings_order = suite_setup,suite_teardown,test_setup,test_teardown,test_timeout,test_template``
+
+    Omitted setting names from custom order will be removed from the file. In following example we are missing metadata
+    therefore all metadata will be removed::
+
+        robotidy --configure OrderSettingsSection:documentation_order=documentation
+
+    Libraries are grouped into built in libraries and custom libraries.
     """
     def __init__(self, new_lines_between_groups: int = 1, group_order: str = None, documentation_order: str = None,
                  imports_order: str = None, settings_order: str = None, tags_order: str = None):
