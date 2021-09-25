d41cda31c2e762b7c21a389197e65d1296c6df70
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-09-25 12:23:29 +0200

    use dedicated class for recreating node

--- robotidy/transformers/OrderTags.py
+++ robotidy/transformers/OrderTags.py
@@ -1,11 +1,11 @@
-from robot.api.parsing import ModelTransformer, Tags, Token
+from robot.api.parsing import ModelTransformer, Tags, Token, DefaultTags, ForceTags
 
 
 class OrderTags(ModelTransformer):
     """
-    Order tags inside Keywords and Test Cases.
+    Order tags.
 
-    Tags are ordered like this:
+    Tags are ordered in lexicographic order like this:
 
         *** Test Cases ***
         Tags Upper Lower
@@ -47,41 +47,50 @@ class OrderTags(ModelTransformer):
         self.default_tags = default_tags
         self.force_tags = force_tags
 
-    def visit_Tags(self, node):
-        return self.order_tags(node)
-
-    def visit_DefaultTags(self, node):
-        return self.order_tags(node) if self.default_tags else node
-
-    def visit_ForceTags(self, node):
-        return self.order_tags(node) if self.force_tags else node
-
-    def order_tags(self, node):
-        tags = [tag.value for tag in node.data_tokens[1:]]
-        if len(tags) > 1:
-            tags = sorted(tags, key=self.key, reverse=self.reverse)
-            tokens = self.get_tokens(tags)
-            node.tokens = Tags(
-                (
-                    *node.tokens[:2],
-                    *tokens,
-                    Token(Token.EOL, '\n'),
-                ),
-            )
+    def visit_Tags(self, node):  # noqa
+        tags = self.order_tags(node)
+        if len(tags) <= 1:
+            return node
+        comments = node.get_tokens(Token.COMMENT)
+        node = Tags.from_params(tags, separator=self.formatting_config.separator)
+        if comments:
+            node.tokens += tuple(self.join_tokens(comments))
         return node
 
-    def get_tokens(self, tags):
-        result = []
-        separator = Token(Token.SEPARATOR, self.formatting_config.separator)
-        for tag in tags:
-            result.extend([separator, Token(Token.TAGS, tag)])
-        return result
+    def visit_DefaultTags(self, node):  # noqa
+        if not self.default_tags:
+            return node
+        tags = self.order_tags(node)
+        if len(tags) <= 1:
+            return node
+        comments = node.get_tokens(Token.COMMENT)
+        node = DefaultTags.from_params(tags, separator=self.formatting_config.separator)
+        if comments:
+            node.tokens += tuple(self.join_tokens(comments))
+        return node
 
-    def get_key(self, case_sensitive):
-        default = str.casefold
-        if case_sensitive == True:
-            return str
-        elif case_sensitive == False:
-            return str.casefold
-        else:
-            return default
+    def visit_ForceTags(self, node):  # noqa
+        if not self.force_tags:
+            return node
+        tags = self.order_tags(node)
+        if len(tags) <= 1:
+            return node
+        comments = node.get_tokens(Token.COMMENT)
+        node = ForceTags.from_params(tags, separator=self.formatting_config.separator)
+        if comments:
+            node.tokens += tuple(self.join_tokens(comments))
+        return node
+
+    def order_tags(self, node):
+        return sorted((tag.value for tag in node.data_tokens[1:]), key=self.key, reverse=self.reverse)
+
+    def join_tokens(self, tokens):
+        joined_tokens = []
+        for token in tokens:
+            joined_tokens.append(Token(Token.SEPARATOR, self.formatting_config.separator))
+            joined_tokens.append(token)
+        return joined_tokens
+
+    @staticmethod
+    def get_key(case_sensitive):
+        return str if case_sensitive else str.casefold
