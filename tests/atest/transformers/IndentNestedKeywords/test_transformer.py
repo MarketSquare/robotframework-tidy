cceffdf85bb8e71bcd704e1529a9c27595193d2b
Bartlomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-06-10 15:43:47 +0200

    Improve handling AND argument in IndentNestedKeywords

--- tests/atest/transformers/IndentNestedKeywords/test_transformer.py
+++ tests/atest/transformers/IndentNestedKeywords/test_transformer.py
@@ -7,8 +7,11 @@ class TestIndentNestedKeywords(TransformerAcceptanceTest):
     def test_run_keyword(self):
         self.compare(source="run_keyword.robot")
 
-    def test_indent_and(self):
-        self.compare(source="run_keyword.robot", expected="indent_and.robot", config=":indent_and=True")
+    def test_split_and_indent(self):
+        self.compare(source="run_keyword.robot", expected="split_and_indent.robot", config=":indent_and=split_and_indent")
+
+    def test_and_keep_in_line(self):
+        self.compare(source="run_keyword.robot", expected="keep_in_line.robot", config=":indent_and=keep_in_line")
 
     def test_indent_and_continuation_indent(self):
         self.compare(
