bd2be1a115cce5bac761f1d4ec3e48f675dc83ba
Bartlomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-06-21 18:45:19 +0200

    Fix missing parameter

--- tests/atest/transformers/IndentNestedKeywords/test_transformer.py
+++ tests/atest/transformers/IndentNestedKeywords/test_transformer.py
@@ -8,7 +8,9 @@ class TestIndentNestedKeywords(TransformerAcceptanceTest):
         self.compare(source="run_keyword.robot")
 
     def test_split_and_indent(self):
-        self.compare(source="run_keyword.robot", expected="split_and_indent.robot", config=":indent_and=split_and_indent")
+        self.compare(
+            source="run_keyword.robot", expected="split_and_indent.robot", config=":indent_and=split_and_indent"
+        )
 
     def test_and_keep_in_line(self):
         self.compare(source="run_keyword.robot", expected="keep_in_line.robot", config=":indent_and=keep_in_line")
@@ -17,7 +19,7 @@ class TestIndentNestedKeywords(TransformerAcceptanceTest):
         self.compare(
             source="run_keyword.robot",
             expected="indent_and_2spaces_4cont_indent.robot",
-            config=":indent_and=True --spacecount 2 --continuation-indent 4",
+            config=":indent_and=split_and_indent --spacecount 2 --continuation-indent 4",
         )
 
     def test_comments(self):
